2a920f1518c77eba02110fb73e29c41ec56c5c69
Patrick Brett <patrickbrett@users.noreply.github.com>
2021-03-07 09:38:49 +1100

    Incorporate request for object keys; map to only send key strings

--- service/api/images.go
+++ service/api/images.go
@@ -8,21 +8,27 @@ import (
 	"fmt"
 )
 
-func listObjects() interface{} {
+func listObjects() []string {
 	bucket := "npb-source-bucket-sourcebucket-96mim45irkcp"
 	prefix := "images/train"
+	keyLimit := int64(1000)
 
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("ap-southeast-2")},
 	)
 
 	client := s3.New(sess)
-	result, err := client.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: aws.String(prefix)})
+	result, err := client.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: aws.String(prefix), MaxKeys: &keyLimit})
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Print(result)
+	keys := make([]string, *result.KeyCount)
+	for i, s := range result.Contents {
+		keys[i] = *s.Key
+	}
 
-	return result
+	fmt.Print(keys)
+
+	return keys
 }
