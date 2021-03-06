ce569ee421021689985edb4dc819ff8ea09544d6
Patrick Brett <patrickbrett@users.noreply.github.com>
2021-03-07 08:35:40 +1100

    Remove SQS related functionality

--- service/api/main.go
+++ service/api/main.go
@@ -6,12 +6,6 @@ import (
 	"net/http"
 
 	"github.com/apex/gateway"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
-	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
-	"github.com/cpliakas/aws-sam-golang-example/job"
-	"github.com/cpliakas/aws-sam-golang-example/lambdautils"
 )
 
 // ContentType contains the Content-Type header sent on all responses.
@@ -22,20 +16,11 @@ type MessageResponse struct {
 	Message string `json:"message"`
 }
 
-// JobResponse models the respose returned by the /job endpoint.
-type JobResponse struct {
-	MessageResponse
-	JobID string `json:"job_id"`
-}
-
 // WelcomeMessageResponse is the response returned by the / endpoint.
 var WelcomeMessageResponse = MessageResponse{"Welcome to the example API!"}
 
 // AltWelcomeMessageResponse is the response returned by the /message endpoint.
 var AltWelcomeMessageResponse = MessageResponse{"Here's another message!"}
-
-// JobMessage is the message sent in JobResponse responses.
-var JobMessage = "Job sent to queue."
 
 // RootHandler is a http.HandlerFunc for the / endpoint.
 func RootHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,46 +32,10 @@ func MessageHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(AltWelcomeMessageResponse)
 }
 
-// JobHandler implements http.Handler for the /job endpoint.
-type JobHandler struct {
-	svc sqsiface.SQSAPI
-}
-
-// JobHandlerFunc returns a http.HandlerFunc for the /job endpoint.
-func JobHandlerFunc(svc sqsiface.SQSAPI) http.HandlerFunc {
-	jh := JobHandler{svc: svc}
-	return jh.ServeHTTP
-}
-
-// JobHandler implements http.Handler for the /job endpoint.
-func (h JobHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	lambdautils.Mustenv(lambdautils.EnvQueueURL)
-	response := sendJobMessage(h.svc, job.LogJobName)
-	json.NewEncoder(w).Encode(response)
-	w.WriteHeader(http.StatusCreated)
-}
-
-// sendJobMessage sends an encoded job.Message to SQS.
-func sendJobMessage(svc sqsiface.SQSAPI, name string) (response JobResponse) {
-
-	output := lambdautils.SendMessage(svc, &sqs.SendMessageInput{
-		QueueUrl:    aws.String(lambdautils.QueueURL()),
-		MessageBody: aws.String(job.NewMessage(name).String()),
-	})
-
-	response = JobResponse{JobID: *output.MessageId}
-	response.Message = JobMessage
-	return
-}
-
 // RegisterRoutes registers the API's routes.
 func RegisterRoutes() {
-	sess := session.Must(session.NewSession())
-	svc := sqs.New(sess)
-
 	http.Handle("/", h(RootHandler))
 	http.Handle("/message", h(MessageHandler))
-	http.Handle("/job", h(JobHandlerFunc(svc)))
 }
 
 // h wraps a http.HandlerFunc and adds common headers.
